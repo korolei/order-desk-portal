3ae3ab2fa2cf0934433c39c6b431dc3f7daa2e74
korolei <ikorolenko@hotmail.com>
2018-12-11 14:17:39 -0500

    Added services & components files.

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrdersComponent } from './orders/orders.component';
+import {OrdersService} from "./orders.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [OrdersComponent],
+  providers: [
+    OrdersService
+  ]
 })
 export class OrdersModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Order} from "./models/order.model";
+import {Quote} from "@angular/compiler";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    Order,
+    Quote
+  ]
 })
 export class SharedModule { }
