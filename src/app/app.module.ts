5ea442e8a8b75be5585de8fd3a1cba3187d66117
korolei <ikorolenko@hotmail.com>
2018-12-11 14:07:47 -0500

    Moved *.module.ts files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AppService} from "./app.service";
+import {AuthModule} from "./auth/auth.module";
+import {OrdersModule} from "./orders/orders.module";
+import {QuotesModule} from "./quotes/quotes.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    OrdersModule,
+    QuotesModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [ AppService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
