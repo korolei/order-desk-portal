4776cfdb3e798afffbe3455ccce6a367bc35b49c
Jack Hanebach <jhanebach@deloitte.com>
2018-12-12 10:11:34 -0500

    moved side-nav to shared, added toolbar elements

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MessageService} from "./services/message.service";
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SideNavComponent
   ],
   providers: [
     MessageService
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -20,7 +20,44 @@ import { map } from 'rxjs/operators';
       position: sticky;
       top: 0;
     }
-        `
+    `,
+    `
+    .husky-icon{
+      padding: 0 14px;
+      color: maroon;
+      font-size: 36px;
+    }
+    `,
+    `
+    .toolbar-spacer{
+      flex: 1 0 auto;
+    }
+    `,
+    `
+    .husky-logo{
+      margin-left: 1em;
+    }
+    `,
+    `
+    .search-autocomplete {
+      border: 1px solid #bcbcbc;
+      border-radius: 4px;
+      position: relative;
+      text-align: left;
+      width: 400px;
+    }
+    `,
+    `
+    .search-button {
+      margin: 3px;
+    }
+    `,
+    `
+    .search-text {
+      margin-left: 6px;
+      width: 290px;
+    }
+    `
   ]
 })
 export class SideNavComponent {
