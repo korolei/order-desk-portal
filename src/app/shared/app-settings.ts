d464ec42cca68f687f6541f070b92f8db495ceda
korolei <ikorolenko@hotmail.com>
2018-12-31 19:55:10 -0500

    Added AppSettings class.

---
+++ src/app/shared/app-settings.ts
@@ -0,0 +1,8 @@
+export class AppSettings {
+  public static organizationApi = 'api/organization';
+  public static installBaseApi = 'api/installBase';
+  public static quickAccountAgingApi = 'api/quickAccountAging';
+  public static caseManagementApi = 'api/caseManagement';
+  public static openQuotationsApi = 'api/quotes';
+  public static openSalesOrderApi = 'api/orders';
+}
